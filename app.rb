81d83b72f6d8704ac7a04852154a528c82bdad15
brianyeh1 <nyeh@chicagobooth.edu>
2025-04-21 03:59:43 +0000

    commit

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user_number").to_f
+  @the_result = @the_num ** 2
+  
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/payment/new") do
+  erb(:payment)
 end
