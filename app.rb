1066cf0dc44e11d21273fd9bd9a51f1af028ab5a
brianyeh1 <nyeh@chicagobooth.edu>
2025-04-21 04:06:53 +0000

    commit

--- app.rb
+++ app.rb
@@ -11,13 +11,20 @@ end
 
 get("/square/results") do
   @the_num = params.fetch("user_number").to_f
-  @the_result = @the_num ** 2
+  @the_results = @the_num ** 2
   
   erb(:square_results)
 end
 
 get("/square_root/new") do
-  erb(:square_root)
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+  @the_results = @the_num ** 0.5
+  
+  erb(:square_root_results)
 end
 
 get("/random/new") do
